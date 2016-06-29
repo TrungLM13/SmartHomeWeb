ddc2d5f3502366139d83f271975ff9b86d47ebbb
TrungLe <trunglm13@gmail.com>
2016-06-29 14:15:37 +0700

    Modify Main

--- smarthomeweb/src/main/java/Handler/Main.java
+++ smarthomeweb/src/main/java/Handler/Main.java
@@ -1,13 +1,28 @@
 package Handler;
 import org.apache.catalina.startup.Tomcat;
 
+import Connector.HiveConnector;
+
 public class Main {
+	
+	private HiveConnector m_conn;
+	
+	public Main(){
+	}
 	
 	public static final String port = System.getenv("PORT");
 	    
     public static void main(String[] args) throws Exception {
         String contextPath = "/";
         String appBase = ".";
+        
+        // Get Data From Hive.
+        Main app = new Main();
+        
+        app.m_conn.CreateConnection();
+        
+        
+        // Start Tomcat
         Tomcat tomcat = new Tomcat();
         if(port != null)
         	tomcat.setPort(Integer.valueOf(8080));
